c3146972a51c2cb71b5b1678f57973b83a5a15e9
VInicius Schuelter <48526428+viniciusschuelter@users.noreply.github.com>
2023-08-30 08:43:12 -0300

    feat(calc): price calculation fixing last details

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.0.
 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/d31fb90e-dd1b-4008-b3af-9c5514bbc07c/deploy-status)](https://app.netlify.com/sites/lucent-kheer-10366b/deploys)
+
+[live demo](https://price-calculation-test.netlify.app)
+
 ## Development server
 
 Run `npm install` and then you can `npm run start` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 
 
 @Component({
@@ -10,9 +10,33 @@ import { Component } from '@angular/core';
         <h1>Produtos</h1>
         <h2>Tela de Cálculo de Preços</h2>
       </hgroup>
+      <a class="theme-switcher c-pointer" (click)="themeSignal.set(themeValues[themeSignal()].changeTo)">
+        <h1>{{themeValues[themeSignal()].icon}}</h1>
+      </a>
     </header>
-  `
+  `,
+  styles: [`
+    :host {
+      header {
+        position: relative;
+        .theme-switcher {
+          position: absolute;
+          top: 1rem;
+          right: 1rem;
+        }
+      }
+    }
+  `]
 })
 export class HeaderComponent {
 
+  themeSignal = signal('light');
+  themeValues: any = { 'light': { icon: '☾', changeTo: 'dark'},  'dark': { icon: '☀', changeTo: 'light'}};
+
+  constructor() {
+    effect( () => {
+      console.log(`theme changed ${this.themeSignal()}`);
+      document.documentElement.setAttribute('data-theme', this.themeSignal());
+    })
+  }
 }
